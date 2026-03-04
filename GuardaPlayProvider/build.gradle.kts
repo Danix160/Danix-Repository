fd6550e0dcbfb0adea2a0303001ba1b170d145df
Danix160 <tiziotipo18@gmail.com>
2026-03-04 18:49:02 +0100

    Update build.gradle.kts

--- GuardaPlayProvider/build.gradle.kts
+++ GuardaPlayProvider/build.gradle.kts
@@ -14,7 +14,7 @@ cloudstream {
     extra["prefix"] = "GuardaPlay"
     extra["displayName"] = "GuardaPlay"
     
-    version = 32
+    version = 33
     description = "GuardaPlay Contiene Solo Film"
     authors = listOf("Danix")
     
