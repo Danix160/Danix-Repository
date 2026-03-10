ce723407489b71badc97bf6171156c9875ad5ee8
Danix160 <tiziotipo18@gmail.com>
2026-03-10 23:29:58 +0100

    Update build.gradle.kts

--- OnlineSerietvProvider/build.gradle.kts
+++ OnlineSerietvProvider/build.gradle.kts
@@ -14,7 +14,7 @@ cloudstream {
     extra["prefix"] = "OnlineSerieTv"
     extra["displayName"] = "OnlineSerieTv"
     
-    version = 14
+    version = 15
     description = "OnlineSerieTv"
     authors = listOf("Danix")
     
