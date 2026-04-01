800df3c21f71c6e52ad34b7d787a63b448dad6da
Danix160 <tiziotipo18@gmail.com>
2026-04-02 00:09:58 +0200

    Update version number to 99

--- CineblogProvider/build.gradle.kts
+++ CineblogProvider/build.gradle.kts
@@ -14,7 +14,7 @@ cloudstream {
     extra["prefix"] = "Cineblog"
     extra["displayName"] = "Cineblog"
     
-    version = 98
+    version = 99
     description = "Cineblog001"
     authors = listOf("Danix")
     
