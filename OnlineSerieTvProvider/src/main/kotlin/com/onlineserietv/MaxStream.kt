0b385cf9da1da4d79f267d8f028c6b3e2133ca9f
Danix160 <tiziotipo18@gmail.com>
2026-05-21 08:38:59 +0200

    Refactor MaxStream extractor to use newExtractorLink

--- OnlineSerieTvProvider/src/main/kotlin/com/onlineserietv/MaxStream.kt
+++ OnlineSerieTvProvider/src/main/kotlin/com/onlineserietv/MaxStream.kt
@@ -4,6 +4,7 @@ import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.utils.newExtractorLink
 
 class MaxStream : ExtractorApi() {
     override val name = "MaxStream"
@@ -15,22 +16,18 @@ class MaxStream : ExtractorApi() {
         referer: String?,
         subtitleCallback: (com.lagradost.cloudstream3.SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
-    ): List<ExtractorLink> {
-        
-        // Eseguiamo la richiesta GET con gli header standard
+    ) {
         val response = app.get(url, referer = referer)
         val html = response.text
 
-        // Regex tradotta dal tuo Python: r'sources\W+src\W+(.*)",'
         val pattern = """sources\W+src\W+([^"\s]+)""".toRegex()
         val match = pattern.find(html)
 
         if (match != null) {
-            // Puliamo l'URL estratto da eventuali virgolette residue
             val videoUrl = match.groupValues[1].replace("\"", "").trim()
 
             callback.invoke(
-                ExtractorLink(
+                newExtractorLink(
                     this.name,
                     this.name,
                     videoUrl,
@@ -40,6 +37,5 @@ class MaxStream : ExtractorApi() {
                 )
             )
         }
-        return emptyList()
     }
 }
