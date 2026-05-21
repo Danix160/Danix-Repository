07c6149fd8a74c14135c6cd35ff92bf409544bf3
Danix160 <tiziotipo18@gmail.com>
2026-05-21 07:25:00 +0200

    Create build.gradle.kts

---
+++ OnlineSerieTvProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+android {
+    // ... altre configurazioni (compileSdk, ecc.) ...
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        // Questa riga dice al compilatore di ignorare che la libreria è "troppo nuova"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+cloudstream {
+    extra["prefix"] = "OnlineSerieTV"
+    extra["displayName"] = "OnlineSerieTV"
+    
+    version = 1
+    description = "OnlineSerieTV Prova"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime", "Cartoon")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://onlineserietv.lol/images/logo.svg"
+}
