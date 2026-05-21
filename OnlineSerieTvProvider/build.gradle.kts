12d66bc0f93b8d24350df88deee98d013f851b9a
Danix160 <tiziotipo18@gmail.com>
2026-05-21 09:17:39 +0200

    Update version number to 3 in build.gradle.kts

--- OnlineSerieTvProvider/build.gradle.kts
+++ OnlineSerieTvProvider/build.gradle.kts
@@ -11,7 +11,7 @@ cloudstream {
     extra["prefix"] = "OnlineSerieTV"
     extra["displayName"] = "OnlineSerieTV"
     
-    version = 2
+    version = 3
     description = "OnlineSerieTV Prova"
     authors = listOf("Danix")
     
