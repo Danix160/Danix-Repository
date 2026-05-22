43ea39cc9c475b02e585c99ba66efa8f695fea97
Danix160 <tiziotipo18@gmail.com>
2026-05-22 07:36:42 +0200

    Create MaxStream.kt

---
+++ CbProvider/src/main/kotlin/com/cb/MaxStream.kt
@@ -0,0 +1,45 @@
+package com.cb
+
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.app
+
+class MaxStream : ExtractorApi() {
+    override val name = "MaxStream"
+    override val mainUrl = "https://maxstream.video"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (com.lagradost.cloudstream3.SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val response = app.get(url, referer = referer)
+        val html = response.text
+
+        val pattern = """sources\W+src\W+([^"\s]+)""".toRegex()
+        val match = pattern.find(html)
+
+        if (match != null) {
+            val videoUrl = match.groupValues[1].replace("\"", "").trim()
+            val isM3u8 = videoUrl.contains(".m3u8")
+
+            // Applichiamo l'esatta sintassi moderna con il blocco lambda configuratore
+            val link = newExtractorLink(
+                source = this.name,
+                name = this.name,
+                url = videoUrl,
+                type = if (isM3u8) ExtractorLinkType.M3U8 else ExtractorLinkType.VIDEO
+            ) {
+                this.referer = url
+                this.quality = Qualities.Unknown.value
+            }
+            
+            callback.invoke(link)
+        }
+    }
+}
