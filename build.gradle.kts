752f6c5f0eae77f030b6083685b5c2b56ba25f0a
Danix160 <tiziotipo18@gmail.com>
2026-06-04 09:19:29 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,59 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://repo.cloudstream.cf/releases")
+        maven("https://jitpack.io")
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:9.1.1")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
+    }
+
+    android {
+        namespace = "com.example"
+        compileSdk = 36
+
+        defaultConfig {
+            minSdk = 21
+        }
+
+        lint {
+            targetSdk = 36
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+
+    dependencies {
+        val cloudstream by configurations
+        val implementation by configurations
+
+        cloudstream("com.lagradost:cloudstream3:pre-release")
+        implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.4.11")
+        implementation("org.jsoup:jsoup:1.18.1")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
     }
 }
