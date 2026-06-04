10c209eb1048738aae81f2a45f0051fcc41a5080
Danix160 <tiziotipo18@gmail.com>
2026-06-04 09:43:13 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:9.1.1")
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
