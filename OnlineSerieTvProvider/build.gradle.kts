86c676f84112d0e053b8a6dce34f57407f6aa78f
Danix160 <tiziotipo18@gmail.com>
2026-06-04 10:11:25 +0200

    Update build.gradle.kts

--- OnlineSerieTvProvider/build.gradle.kts
+++ OnlineSerieTvProvider/build.gradle.kts
@@ -1,23 +1,6 @@
-apply(plugin = "com.android.library")
-apply(plugin = "kotlin-android")
-apply(plugin = "com.lagradost.cloudstream3.gradle")
-
 android {
-    namespace = "com.onlineserietv"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 21
-    }
     kotlinOptions {
         jvmTarget = "1.8"
-        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
-    }
-    // ... altre configurazioni (compileSdk, ecc.) ...
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-        // Questa riga dice al compilatore di ignorare che la libreria è "troppo nuova"
         freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
     }
 }
