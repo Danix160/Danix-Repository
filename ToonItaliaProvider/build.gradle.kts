44c787f2a7b7407b7da204fd41be6f88ca9688a7
Danix160 <tiziotipo18@gmail.com>
2026-06-04 10:00:28 +0200

    Update build.gradle.kts

--- ToonItaliaProvider/build.gradle.kts
+++ ToonItaliaProvider/build.gradle.kts
@@ -1,9 +1,5 @@
-apply(plugin = "com.android.library")
-apply(plugin = "kotlin-android")
-apply(plugin = "com.lagradost.cloudstream3.gradle")
-
 android {
-    namespace = "com.altadefinizione"
+    namespace = "com.toonitalia"
     compileSdk = 35
 
     defaultConfig {
