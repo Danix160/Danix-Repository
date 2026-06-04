8e7d7c4b22426569e28c45a2b837c403d392a046
Danix160 <tiziotipo18@gmail.com>
2026-06-04 09:59:19 +0200

    Update build.gradle.kts

--- AltaDefinizioneProvider/build.gradle.kts
+++ AltaDefinizioneProvider/build.gradle.kts
@@ -1,7 +1,3 @@
-apply(plugin = "com.android.library")
-apply(plugin = "kotlin-android")
-apply(plugin = "com.lagradost.cloudstream3.gradle")
-
 android {
     namespace = "com.altadefinizione"
     compileSdk = 35
