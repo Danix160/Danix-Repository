a9835d6706b7713c579251b0534eefe7817bdbd9
Danix160 <tiziotipo18@gmail.com>
2026-06-04 11:03:20 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,12 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://repo.cloudstream.cf/releases")
+        maven("https://jitpack.io")
     }
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("com.lagradost.cloudstream3:gradle:0.0.1")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     }
 }
@@ -21,7 +21,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://repo.cloudstream.cf/releases")
+        maven("https://jitpack.io")
     }
 }
 
