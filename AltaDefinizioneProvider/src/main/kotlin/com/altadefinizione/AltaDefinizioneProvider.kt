31a511240a154fa418901fe5cea230df21fe8459
Danix160 <tiziotipo18@gmail.com>
2026-06-08 18:27:22 +0200

    Update AltaDefinizioneProvider.kt

--- AltaDefinizioneProvider/src/main/kotlin/com/altadefinizione/AltaDefinizioneProvider.kt
+++ AltaDefinizioneProvider/src/main/kotlin/com/altadefinizione/AltaDefinizioneProvider.kt
@@ -2,6 +2,7 @@ package com.altadefinizione
 
 import org.jsoup.nodes.Element
 import org.json.JSONObject
+import org.jsoup.Jsoup
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
