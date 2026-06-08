577695ed78a2bc92f21ebd6089dee01f3577bbf8
Danix160 <tiziotipo18@gmail.com>
2026-06-08 19:02:55 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,28 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io") // Permette a Gradle di trovare il plugin di Cloudstream a livello globale
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PRESERVE_EXISTING)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
 rootProject.name = "CloudstreamPlugins"
 
+// Lista dei provider che vuoi disabilitare/ignorare temporaneamente
 val disabled = listOf<String>()
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
+        include(":${dir.name}") // Corretto l'include aggiungendo il flag standard ":" di Gradle
     }
 }
 
