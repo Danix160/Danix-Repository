a2f45e4eac4c1f7676f116747806608fb65841eb
Danix160 <tiziotipo18@gmail.com>
2026-06-14 10:53:06 +0200

    Update build.gradle.kts

--- NtvProvider/build.gradle.kts
+++ NtvProvider/build.gradle.kts
@@ -11,7 +11,7 @@ cloudstream {
     extra["prefix"] = "Ntv.cx"
     extra["displayName"] = "Ntv.cx"
     
-    version = 1
+    version = 2
     description = "Ntv.cx Stream Live"
     authors = listOf("Danix")
     
