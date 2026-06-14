648244c48225b68449e6bf3d64511f713c06f511
Danix160 <tiziotipo18@gmail.com>
2026-06-14 12:56:08 +0200

    Create build.gradle.kts

---
+++ MultiXtreamProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+android {
+
+    namespace = "com.multixtream"
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+cloudstream {
+    extra["prefix"] = "MultiXtream"
+    extra["displayName"] = "MultiXtream"
+    
+    version = 1
+    description = "MultiXtream IPTV LIVE"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Live")
+    requiresResources = false
+    language = "it"
+    iconUrl = "http://vegetatv.duckdns.org/vegeta_cyan.webp"
+}
