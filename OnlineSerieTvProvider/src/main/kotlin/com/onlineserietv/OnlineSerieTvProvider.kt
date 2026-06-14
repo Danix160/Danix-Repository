b8d9cb32834e7374234d03c4678bafb38e135795
Danix160 <tiziotipo18@gmail.com>
2026-06-14 09:24:02 +0200

    Update OnlineSerieTvProvider.kt

--- OnlineSerieTvProvider/src/main/kotlin/com/onlineserietv/OnlineSerieTvProvider.kt
+++ OnlineSerieTvProvider/src/main/kotlin/com/onlineserietv/OnlineSerieTvProvider.kt
@@ -181,9 +181,30 @@ class OnlineSerieTvProvider : MainAPI() {
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 
     override val mainPage = mainPageOf(
-        "$mainUrl/movies" to "Film",
-        "$mainUrl/serie-tv/" to "Serie TV",
-        "$mainUrl/serie-tv-generi/animazione/" to "Cartoni & Anime"
+        "$mainUrl/movies/" to "Film: Ultimi aggiunti",
+        "$mainUrl/serie-tv/" to "Serie TV: Ultime aggiunte",
+
+        "$mainUrl/serie-tv-generi/animazione/" to "Serie TV: Animazione",
+        "$mainUrl/film-generi/animazione/" to "Film: Animazione",
+
+        "$mainUrl/serie-tv-generi/action-adventure/" to "Serie TV: Azione e Avventura",
+        "$mainUrl/film-generi/avventura/" to "Film: Avventura",
+        "$mainUrl/film-generi/azione/" to "Film: Azione",
+        "$mainUrl/film-generi/supereroi/" to "Film: Supereroi",
+
+        "$mainUrl/serie-tv-generi/sci-fi-fantasy/" to "Serie TV: Fantascienza e Fantasy",
+        "$mainUrl/film-generi/fantascienza/" to "Film: Fantascienza",
+        "$mainUrl/film-generi/fantasy/" to "Film: Fantasy",
+
+        "$mainUrl/serie-tv-generi/commedia/" to "Serie TV: Commedia",
+        "$mainUrl/film-generi/commedia/" to "Film: Commedia",
+
+        "$mainUrl/serie-tv-generi/crime/" to "Serie TV: Crime",
+        "$mainUrl/serie-tv-generi/mistero/" to "Serie TV: Mistero",
+
+        "$mainUrl/film-generi/horror/" to "Film: Horror",
+        "$mainUrl/film-generi/thriller/" to "Film: Thriller",
+
     )
 
     // -----------------------------
