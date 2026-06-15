0fce8f25b659922e108b0e5f221a83ea484d40f5
Danix160 <tiziotipo18@gmail.com>
2026-06-15 08:03:01 +0200

    Update build.gradle.kts

--- MultiXtreamProvider/build.gradle.kts
+++ MultiXtreamProvider/build.gradle.kts
@@ -11,7 +11,7 @@ cloudstream {
     extra["prefix"] = "MultiXtream"
     extra["displayName"] = "MultiXtream"
     
-    version = 16
+    version = 17
     description = "MultiXtream IPTV LIVE"
     authors = listOf("Danix")
     
