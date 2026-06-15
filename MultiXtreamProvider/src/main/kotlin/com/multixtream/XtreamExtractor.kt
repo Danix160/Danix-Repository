1ddfa772d0545378ea3217ef16133d15fd84c252
Danix160 <tiziotipo18@gmail.com>
2026-06-15 08:14:12 +0200

    Update XtreamExtractor.kt

--- MultiXtreamProvider/src/main/kotlin/com/multixtream/XtreamExtractor.kt
+++ MultiXtreamProvider/src/main/kotlin/com/multixtream/XtreamExtractor.kt
@@ -1,11 +1,9 @@
 package com.multixtream
 
-import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
-import com.lagradost.cloudstream3.utils.newExtractorLink
 
 class XtreamExtractor : ExtractorApi() {
 
@@ -23,25 +21,15 @@ class XtreamExtractor : ExtractorApi() {
         val cleanUrl = url.trim()
         val isM3u8 = cleanUrl.contains(".m3u8")
 
-        // Headers intelligenti per Xtream
-        val headers = mapOf(
-            "User-Agent" to "Mozilla/5.0",
-            "Accept" to "*/*",
-            "Connection" to "keep-alive"
+        callback(
+            ExtractorLink(
+                source = name,
+                name = "Xtream Live",
+                url = cleanUrl,
+                referer = null,
+                quality = Qualities.Unknown.value,
+                isM3u8 = isM3u8
+            )
         )
-
-        val link = newExtractorLink(
-            source = this.name,
-            name = this.name,
-            url = cleanUrl,
-            type = if (isM3u8) ExtractorLinkType.M3U8 else ExtractorLinkType.VIDEO
-        ) {
-            this.headers = headers
-            this.referer = cleanUrl.substringBefore("/live/")
-            this.isM3u8 = isM3u8
-            this.quality = Qualities.Unknown.value
-        }
-
-        callback.invoke(link)
     }
 }
