ab03aae682b7383f4467f60ea85e60e6784a5bf2
Danix160 <tiziotipo18@gmail.com>
2026-06-16 09:01:04 +0200

    Update MultiXtreamPlugin.kt

--- MultiXtreamProvider/src/main/kotlin/com/multixtream/MultiXtreamPlugin.kt
+++ MultiXtreamProvider/src/main/kotlin/com/multixtream/MultiXtreamPlugin.kt
@@ -6,26 +6,45 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.ui.settings.SettingsProvider
 import com.lagradost.cloudstream3.ui.settings.SelectSetting
 
+
 @CloudstreamPlugin
 class MultiXtreamPlugin : Plugin() {
 
     override fun load(context: Context) {
 
-        // Impostazione ufficiale Cloudstream 4
+
         SettingsProvider.addSetting(
             SelectSetting(
                 key = "multixtream_version",
                 title = "Versione Provider",
-                values = listOf("v1", "v2"),
+                values = listOf(
+                    "v1",
+                    "v2"
+                ),
                 defaultValue = "v1"
             )
         )
 
-        val version = SettingsProvider.getString("multixtream_version", "v1")
 
-        when (version) {
-            "v2" -> registerMainAPI(MultiXtreamProviderV2())
-            else -> registerMainAPI(MultiXtreamProvider())
+        when(
+            SettingsProvider.getString(
+                "multixtream_version",
+                "v1"
+            )
+        ) {
+
+            "v2" -> {
+                registerMainAPI(
+                    MultiXtreamProviderV2()
+                )
+            }
+
+
+            else -> {
+                registerMainAPI(
+                    MultiXtreamProvider()
+                )
+            }
         }
     }
 }
