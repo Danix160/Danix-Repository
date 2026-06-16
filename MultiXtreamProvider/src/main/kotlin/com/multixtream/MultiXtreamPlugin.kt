09923d14251f20c1cce45de065d8b3d89d89347b
Danix160 <tiziotipo18@gmail.com>
2026-06-16 10:24:13 +0200

    Update MultiXtreamPlugin.kt

--- MultiXtreamProvider/src/main/kotlin/com/multixtream/MultiXtreamPlugin.kt
+++ MultiXtreamProvider/src/main/kotlin/com/multixtream/MultiXtreamPlugin.kt
@@ -1,21 +1,33 @@
 package com.multixtream
 
 import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.CommonActivity.activity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class MultiXtreamPlugin : Plugin() {
 
-    override fun load(context: Context) {
+    private val sharedPref by lazy {
+        activity?.getSharedPreferences("MultiXtream", Context.MODE_PRIVATE)
+    }
 
-        val version = getKey<String>("multixtream_version") ?: "v1"
+    override fun load(context: Context) {
+        val version = sharedPref?.getString("version", "v1") ?: "v1"
 
         when (version) {
             "v2" -> registerMainAPI(MultiXtreamProviderV2())
             else -> registerMainAPI(MultiXtreamProvider())
         }
 
-        registerSettings(MultiXtreamSettings())
+        // eventuali extractor
+        // registerExtractorAPI(XtreamExtractor())
+
+        openSettings = { ctx ->
+            val act = ctx as AppCompatActivity
+            val frag = MultiXtreamSettings(this, sharedPref)
+            frag.show(act.supportFragmentManager, "MultiXtreamSettings")
+        }
     }
 }
