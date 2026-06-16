1d54e26956d923616e636e1254a54de919466930
Danix160 <tiziotipo18@gmail.com>
2026-06-16 09:53:19 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,12 @@ buildscript {
     }
 }
 
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+
 allprojects {
     repositories {
         google()
