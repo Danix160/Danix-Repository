c92ac3d5f5ea0f43d98b3e3912b0e48cc08be6b4
Danix160 <tiziotipo18@gmail.com>
2026-06-16 08:14:55 +0200

    Update MultiXtreamPlugin.kt

--- MultiXtreamProvider/src/main/kotlin/com/multixtream/MultiXtreamPlugin.kt
+++ MultiXtreamProvider/src/main/kotlin/com/multixtream/MultiXtreamPlugin.kt
@@ -3,12 +3,33 @@ package com.multixtream // Deve essere uguale a quello in cima al file
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import android.content.SharedPreferences
+import androidx.fragment.app.Fragment
 
 @CloudstreamPlugin
-class MultiXtreamPlugin: Plugin() {
+class MultiXtreamPlugin : Plugin() {
+
+    companion object {
+        const val PREF_NAME = "multixtream_settings"
+        const val KEY_SITE_VERSION = "site_version"
+        const val KEY_VERSION_POSITION = "versionPosition"
+    }
+
     override fun load(context: Context) {
-        // Registra il provider definito sopra nella classe ToonItaliaProvider
-        registerMainAPI(MultiXtreamProvider())
-        registerExtractorAPI(XtreamExtractor())
+        val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val version = prefs.getString(KEY_SITE_VERSION, "v1") ?: "v1"
+
+        when (version) {
+            "v1" -> registerMainAPI(MultiXtreamProvider())
+            "v2" -> registerMainAPI(MultiXtreamProviderV2())
+            "v3" -> registerMainAPI(MultiXtreamProviderV3())
+            else -> registerMainAPI(MultiXtreamProvider())
+        }
+    }
+
+    override fun getSettingsFragment(context: Context): Fragment {
+        val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        return MultiXtreamSettings(this, prefs)
     }
 }
+
