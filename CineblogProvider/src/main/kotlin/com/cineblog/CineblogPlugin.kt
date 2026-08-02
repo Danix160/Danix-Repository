4fb65a44ef1449fc05c601d49ffe1db4b3b02fc4
Danix160 <tiziotipo18@gmail.com>
2026-08-02 13:43:46 +0200

    Update CineblogPlugin.kt

--- CineblogProvider/src/main/kotlin/com/cineblog/CineblogPlugin.kt
+++ CineblogProvider/src/main/kotlin/com/cineblog/CineblogPlugin.kt
@@ -3,7 +3,7 @@ package com.cineblog // Deve essere uguale a quello in cima al file
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
-import com.cineblog.extractor
+import com.cineblog.extractor.VidxGoExtractor
 
 @CloudstreamPlugin
 class CineblogPlugin: Plugin() {
