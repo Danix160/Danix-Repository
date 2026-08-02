58fbc6bea8a058585209c130457622f765d61024
Danix160 <tiziotipo18@gmail.com>
2026-08-02 08:06:35 +0200

    Create build.gradle.kts

---
+++ AltaDefinizioneProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+android {
+
+    namespace = "com.altadefinizione"
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+cloudstream {
+    extra["prefix"] = "AltaDefinizione"
+    extra["displayName"] = "AltaDefinizione"
+    
+    version = 1
+    description = "AltaDefinizione BETA"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://altadefinizionex.co/templates/altadefinizione/images/logo.svg"
+}
