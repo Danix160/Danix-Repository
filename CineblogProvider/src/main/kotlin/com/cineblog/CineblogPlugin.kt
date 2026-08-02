a2316a651af4127d98021ad927b5033743226848
Danix160 <tiziotipo18@gmail.com>
2026-08-02 13:31:22 +0200

    Create CineblogPlugin.kt

---
+++ CineblogProvider/src/main/kotlin/com/cineblog/CineblogPlugin.kt
@@ -0,0 +1,15 @@
+package com.cineblog // Deve essere uguale a quello in cima al file
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.cineblog.extractor
+
+@CloudstreamPlugin
+class CineblogPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Registra il provider definito sopra nella classe ToonItaliaProvider
+        registerMainAPI(CineblogProvider())
+        registerExtractorAPI(VidxGoExtractor())
+    }
+}
