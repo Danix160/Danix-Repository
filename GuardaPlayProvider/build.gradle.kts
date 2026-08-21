1148833bf3ef9e2f0a7842d1687ec3d9ff7b860e
Danix160 <tiziotipo18@gmail.com>
2026-08-22 00:35:21 +0200

    Add build.gradle.kts for GuardaPlayProvider setup

---
+++ GuardaPlayProvider/build.gradle.kts
@@ -0,0 +1,29 @@
+android {
+
+    namespace = "com.guardaplay"
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
+
+cloudstream {
+    extra["prefix"] = "GuardaPlay"
+    extra["displayName"] = "GuardaPlay"
+    
+    version = 1
+    description = "GuardaPlay"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://guardaplay.online/wp-content/uploads/2026/06/cropped-GuardaPlay.png"
+}
