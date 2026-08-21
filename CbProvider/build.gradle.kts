f638705a1fe2734a24a371915ea5861c42a53b29
Danix160 <tiziotipo18@gmail.com>
2026-08-21 23:00:04 +0200

    Update version from 72 to 73 in build.gradle.kts

--- CbProvider/build.gradle.kts
+++ CbProvider/build.gradle.kts
@@ -18,7 +18,7 @@ cloudstream {
     extra["prefix"] = "CB01"
     extra["displayName"] = "CB01"
     
-    version = 72
+    version = 73
     description = "cb01uno.one"
     authors = listOf("Danix")
     
