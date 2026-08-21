1681d3cda0e00d6cdd1eb146d76ab97e17251f27
Danix160 <tiziotipo18@gmail.com>
2026-08-21 23:57:40 +0200

    Create Altadefinizione01Plugin.kt

---
+++ Altadefinizione01Provider/src/main/kotlin/com/altadefinizione01/Altadefinizione01Plugin.kt
@@ -0,0 +1,24 @@
+package com.altadefinizione01
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class Altadefinizione01Plugin : Plugin() {
+
+    override fun load(context: Context) {
+
+        registerMainAPI(
+            Altadefinizione01Provider()
+        )
+
+        /*
+         * Lo aggiungeremo nel prossimo passaggio:
+         *
+         * registerExtractorAPI(
+         *     VidxGoExtractor()
+         * )
+         */
+    }
+}
