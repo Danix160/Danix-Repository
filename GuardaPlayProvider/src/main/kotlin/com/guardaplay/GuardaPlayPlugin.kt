184bf727b4d76a460ee45c11faebf8f2028a6cc7
Danix160 <tiziotipo18@gmail.com>
2026-08-22 00:53:30 +0200

    Update GuardaPlayPlugin.kt

--- GuardaPlayProvider/src/main/kotlin/com/guardaplay/GuardaPlayPlugin.kt
+++ GuardaPlayProvider/src/main/kotlin/com/guardaplay/GuardaPlayPlugin.kt
@@ -1,11 +1,22 @@
 package com.guardaplay
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class GuardaPlayPlugin : Plugin() {
-    override fun load(context: android.content.Context) {
-        registerMainAPI(GuardaPlayProvider())
+
+    override fun load(
+        context: Context
+    ) {
+
+        registerMainAPI(
+            GuardaPlayProvider()
+        )
+
+        registerExtractorAPI(
+            LoadmExtractor()
+        )
     }
 }
