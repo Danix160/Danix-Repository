ec85ac8db5e3a57516e5033f78aa92fc557ea51a
Danix160 <tiziotipo18@gmail.com>
2026-08-21 19:25:19 +0200

    Update build.gradle.kts

--- OnlineSerieTvProvider/build.gradle.kts
+++ OnlineSerieTvProvider/build.gradle.kts
@@ -11,6 +11,7 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    compileOnly("androidx.fragment:fragment-ktx:1.8.5")
 }
 
 cloudstream {
