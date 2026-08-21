6631b4fa6d3af7c0664b82136f395b20300020f6
Danix160 <tiziotipo18@gmail.com>
2026-08-21 23:56:08 +0200

    Create build.gradle.kts

---
+++ Altadefinizione01Provider/build.gradle.kts
@@ -0,0 +1,29 @@
+android {
+
+    namespace = "com.altadefinizione01"
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
+
+cloudstream {
+    extra["prefix"] = "Altadefinizione01"
+    extra["displayName"] = "Altadefinizione01"
+    
+    version = 1
+    description = "Altadefinizione"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Cartoon")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://altadefinizione-01.fun/templates/altadefinizione01/images/logo.png"
+}
