281378a99a205e2792ac193cf1af655b4857d06f
Danix160 <tiziotipo18@gmail.com>
2026-08-25 08:40:56 +0200

    Create build.gradle.kts

---
+++ UniversalProvider/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+    language = "it"
+
+    description =
+        "Catalogo universale con sorgenti multiple"
+
+    authors =
+        listOf(
+            "Danix"
+        )
+
+    status = 1
+
+    tvTypes =
+        listOf(
+            "Movie",
+            "TvSeries"
+        )
+
+    iconUrl =
+        "https://www.google.com/s2/favicons?domain=themoviedb.org&sz=%size%"
+}
