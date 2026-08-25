8a9b8bdcae1800b39ea5fac6b8ef0e6fa6533089
Danix160 <tiziotipo18@gmail.com>
2026-08-25 08:54:33 +0200

    Update load method to use android.content.Context
    Refactor load method to use imported Context.

--- UniversalProvider/src/main/kotlin/com/universal/UniversalPlugin.kt
+++ UniversalProvider/src/main/kotlin/com/universal/UniversalPlugin.kt
@@ -1,15 +1,23 @@
 package com.universal
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.universal.extractors.VidxGoExtractor
 
 @CloudstreamPlugin
 class UniversalPlugin : Plugin() {
 
-    override fun load(context: android.content.Context) {
+    override fun load(context: Context) {
 
+        // Provider principale
         registerMainAPI(
             UniversalProvider()
+        )
+
+        // Extractor VidxGo usato da Altadefinizione01
+        registerExtractorAPI(
+            VidxGoExtractor()
         )
     }
 }
