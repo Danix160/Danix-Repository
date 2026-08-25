3752995126c25967f29b22bb33724e75e0bd0d15
Danix160 <tiziotipo18@gmail.com>
2026-08-25 12:24:14 +0200

    Update ProviderEpisode.kt

--- UniversalProvider/src/main/kotlin/com/universal/models/ProviderEpisode.kt
+++ UniversalProvider/src/main/kotlin/com/universal/models/ProviderEpisode.kt
@@ -8,6 +8,22 @@ data class ProviderEpisode(
 
     val episode: Int? = null,
 
+    /*
+     * Parte/segmento dell'episodio.
+     *
+     * Esempi CB01:
+     * 1x01.1 -> part = 1
+     * 1x01.2 -> part = 2
+     */
+    val part: Int? = null,
+
+    /*
+     * Posizione reale nella sequenza del provider.
+     *
+     * Importante:
+     * per provider con episodi spezzati,
+     * ogni segmento incrementa questo valore.
+     */
     val absoluteEpisode: Int,
 
     val title: String? = null,
