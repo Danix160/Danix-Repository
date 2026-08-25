f4b775373911da5b0165048e4329294ae22fa31b
Danix160 <tiziotipo18@gmail.com>
2026-08-25 08:58:25 +0200

    Update build.gradle.kts

--- UniversalProvider/build.gradle.kts
+++ UniversalProvider/build.gradle.kts
@@ -1,24 +1,29 @@
-version = 2
+android {
+
+    namespace = "com.universal"
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
 
 cloudstream {
-    language = "it"
-
-    description =
-        "Catalogo universale con sorgenti multiple"
-
-    authors =
-        listOf(
-            "Danix"
-        )
-
+    extra["prefix"] = "Universal"
+    extra["displayName"] = "Universal"
+    
+    version = 2
+    description = "Server Multiplo per Film, Serie TV e Cartoni"
+    authors = listOf("Danix")
+    
     status = 1
-
-    tvTypes =
-        listOf(
-            "Movie",
-            "TvSeries"
-        )
-
-    iconUrl =
-        "https://www.google.com/s2/favicons?domain=themoviedb.org&sz=%size%"
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://www.google.com/s2/favicons?domain=themoviedb.org&sz=%size%"
 }
