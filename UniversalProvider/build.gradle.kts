dc3245803993204c58361a6ea35598c49e2ae777
Danix160 <tiziotipo18@gmail.com>
2026-08-25 13:44:50 +0200

    Update version from 16 to 17 in build.gradle.kts

--- UniversalProvider/build.gradle.kts
+++ UniversalProvider/build.gradle.kts
@@ -17,7 +17,7 @@ cloudstream {
     extra["prefix"] = "Universal"
     extra["displayName"] = "Universal"
     
-    version = 16
+    version = 17
     description = "Server Multiplo per Film, Serie TV e Cartoni"
     authors = listOf("Danix")
     
