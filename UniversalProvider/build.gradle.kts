7d900feb9e08688583a6f2f96cede2053331b36e
Danix160 <tiziotipo18@gmail.com>
2026-08-25 13:52:34 +0200

    Update build.gradle.kts

--- UniversalProvider/build.gradle.kts
+++ UniversalProvider/build.gradle.kts
@@ -17,7 +17,7 @@ cloudstream {
     extra["prefix"] = "Universal"
     extra["displayName"] = "Universal"
     
-    version = 17
+    version = 18
     description = "Server Multiplo per Film, Serie TV e Cartoni"
     authors = listOf("Danix")
     
