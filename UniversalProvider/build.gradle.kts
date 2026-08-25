65c8e95062fb338841f2a60cd9dfffce14cffa3a
Danix160 <tiziotipo18@gmail.com>
2026-08-25 09:41:58 +0200

    Update version from 4 to 5 in build.gradle.kts

--- UniversalProvider/build.gradle.kts
+++ UniversalProvider/build.gradle.kts
@@ -17,7 +17,7 @@ cloudstream {
     extra["prefix"] = "Universal"
     extra["displayName"] = "Universal"
     
-    version = 4
+    version = 5
     description = "Server Multiplo per Film, Serie TV e Cartoni"
     authors = listOf("Danix")
     
