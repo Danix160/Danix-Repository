bb53047988ac967a9384afd1f2b9ec41a0b2dd60
Danix160 <tiziotipo18@gmail.com>
2026-08-26 11:08:56 +0200

    Bump version from 84 to 85 in build.gradle.kts

--- CbProvider/build.gradle.kts
+++ CbProvider/build.gradle.kts
@@ -18,7 +18,7 @@ cloudstream {
     extra["prefix"] = "CB01"
     extra["displayName"] = "CB01"
     
-    version = 84
+    version = 85
     description = "cb01uno.one"
     authors = listOf("Danix")
     
