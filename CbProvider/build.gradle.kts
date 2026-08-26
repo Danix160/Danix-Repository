3d56943ce2f17d02494ba07be45460ffcdb7a474
Danix160 <tiziotipo18@gmail.com>
2026-08-26 12:08:27 +0200

    Update version from 87 to 88 in build.gradle.kts

--- CbProvider/build.gradle.kts
+++ CbProvider/build.gradle.kts
@@ -18,7 +18,7 @@ cloudstream {
     extra["prefix"] = "CB01"
     extra["displayName"] = "CB01"
     
-    version = 87
+    version = 88
     description = "cb01uno.one"
     authors = listOf("Danix")
     
