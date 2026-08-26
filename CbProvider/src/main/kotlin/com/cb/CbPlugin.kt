3e37772b7be3d1313ccbd88c403061fbd37e1c94
Danix160 <tiziotipo18@gmail.com>
2026-08-26 09:38:15 +0200

    Update CbPlugin.kt

--- CbProvider/src/main/kotlin/com/cb/CbPlugin.kt
+++ CbProvider/src/main/kotlin/com/cb/CbPlugin.kt
@@ -1,15 +1,22 @@
-package com.cb // Deve essere uguale a quello in cima al file
+package com.cb
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class CbPlugin: Plugin() {
+class CbPlugin : Plugin() {
+
     override fun load(context: Context) {
-        // Registra il provider definito sopra nella classe ToonItaliaProvider
+
+        // Registra l'Activity per entrambe le WebView
         UprotWebView.setContext(context)
+        MaxStreamWebView.setContext(context)
+
+        // Provider
         registerMainAPI(CbProvider())
+
+        // Extractor
         registerExtractorAPI(Uprot())
         registerExtractorAPI(MaxStream())
     }
