88b21d447e928179a2ce54d1b4b3731c5562f881
Danix160 <tiziotipo18@gmail.com>
2026-08-27 10:28:12 +0200

    Update version from 20 to 21 in build.gradle.kts

--- UniversalProvider/build.gradle.kts
+++ UniversalProvider/build.gradle.kts
@@ -17,7 +17,7 @@ cloudstream {
     extra["prefix"] = "Universal"
     extra["displayName"] = "Universal"
     
-    version = 20
+    version = 21
     description = "Server Multiplo per Film, Serie TV e Cartoni"
     authors = listOf("Danix")
     
