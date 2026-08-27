5c959b060fd237bbb5b1cb24eef19fcf9bec3f04
Danix160 <tiziotipo18@gmail.com>
2026-08-27 13:43:52 +0200

    Create RiveStreamPlugin.kt

---
+++ RiveStreamProvider/src/main/kotlin/com/rivestream/RiveStreamPlugin.kt
@@ -0,0 +1,15 @@
+package com.rivestream
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class RiveStreamPlugin : Plugin() {
+
+    override fun load(context: Context) {
+        registerMainAPI(
+            RiveStreamProvider()
+        )
+    }
+}
