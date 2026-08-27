758015b0c4227becb50e8e24b70c5b2bb673d905
Danix160 <tiziotipo18@gmail.com>
2026-08-27 13:43:16 +0200

    Create build.gradle.kts

---
+++ RiveStreamProvider/build.gradle.kts
@@ -0,0 +1,29 @@
+android {
+
+    namespace = "com.rivestream"
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
+
+cloudstream {
+    extra["prefix"] = "RiveStream"
+    extra["displayName"] = "RiveStream"
+    
+    version = 1
+    description = "Live Events Sport "
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Live")
+    requiresResources = false
+    language = "it"
+    iconUrl = ""
+}
