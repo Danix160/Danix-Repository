bc96b17d83ffb26df972f2caea28df0d97430b64
Danix160 <tiziotipo18@gmail.com>
2026-08-28 12:14:09 +0200

    Update version from 18 to 20 in build.gradle.kts

--- RiveStreamProvider/build.gradle.kts
+++ RiveStreamProvider/build.gradle.kts
@@ -17,7 +17,7 @@ cloudstream {
     extra["prefix"] = "RiveStream"
     extra["displayName"] = "RiveStream"
     
-    version = 18
+    version = 20
     description = "Live Events Sport "
     authors = listOf("Danix")
     
