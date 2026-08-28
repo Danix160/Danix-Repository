1321850344a8512fc3c0655e39484b9a1e339a7f
Danix160 <tiziotipo18@gmail.com>
2026-08-28 14:01:39 +0200

    Update build.gradle.kts

--- RiveStreamProvider/build.gradle.kts
+++ RiveStreamProvider/build.gradle.kts
@@ -17,7 +17,7 @@ cloudstream {
     extra["prefix"] = "RiveStream"
     extra["displayName"] = "RiveStream"
     
-    version = 25
+    version = 26
     description = "Live Events Sport "
     authors = listOf("Danix")
     
