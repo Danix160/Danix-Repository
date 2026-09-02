0f20f0d73e0feee5ac00dd5d963f69461a436ac6
Danix160 <tiziotipo18@gmail.com>
2026-09-02 19:15:35 +0200

    Update build.gradle.kts

--- LoonexProvider/build.gradle.kts
+++ LoonexProvider/build.gradle.kts
@@ -16,7 +16,7 @@ cloudstream {
     extra["prefix"] = "Loonex"
     extra["displayName"] = "Loonex"
     
-    version = 3
+    version = 4
     description = "Archivio di Anime e Cartoni animati in italiano da Loonex"
     authors = listOf("Danix")
     
