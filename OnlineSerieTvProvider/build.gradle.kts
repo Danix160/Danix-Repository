62745a2f6c81b9b92d174f9131cef5c98ec128b5
Danix160 <tiziotipo18@gmail.com>
2026-09-02 18:04:03 +0200

    Update build.gradle.kts

--- OnlineSerieTvProvider/build.gradle.kts
+++ OnlineSerieTvProvider/build.gradle.kts
@@ -1,11 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 android {
     namespace = "com.onlineserietv"
-    
-    kotlinOptions {
-        jvmTarget = "1.8"
-        freeCompilerArgs = freeCompilerArgs + "-Xskip-metadata-version-check"
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_1_8)
+        freeCompilerArgs.add("-Xskip-metadata-version-check")
     }
 }
+
 
 // ⬇️ LE DIPENDENZE VANNO RACCHIUSE IN QUESTO BLOCCO
 dependencies {
