4ccac7d26415034bdc444e0f52249d1eea2fd241
Danix160 <tiziotipo18@gmail.com>
2026-09-03 06:51:13 +0200

    Update version from 11 to 12 in build.gradle.kts

--- LoonexProvider/build.gradle.kts
+++ LoonexProvider/build.gradle.kts
@@ -16,7 +16,7 @@ cloudstream {
     extra["prefix"] = "Loonex"
     extra["displayName"] = "Loonex"
     
-    version = 11
+    version = 12
     description = "Archivio di Anime e Cartoni animati in italiano da Loonex"
     authors = listOf("Danix")
     
