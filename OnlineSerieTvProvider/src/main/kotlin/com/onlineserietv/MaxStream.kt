cc87b59184d595a935656ecb39105b0eb5d3b2ad
Danix160 <tiziotipo18@gmail.com>
2026-09-03 12:59:15 +0200

    Update MaxStream.kt

--- OnlineSerieTvProvider/src/main/kotlin/com/onlineserietv/MaxStream.kt
+++ OnlineSerieTvProvider/src/main/kotlin/com/onlineserietv/MaxStream.kt
@@ -7,6 +7,8 @@ import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.M3u8Helper
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.newExtractorLink
+import android.webkit.CookieManager
+import android.util.Log
 
 class MaxStream : ExtractorApi() {
     override val name = "MaxStream"
@@ -19,12 +21,40 @@ class MaxStream : ExtractorApi() {
         subtitleCallback: (com.lagradost.cloudstream3.SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val headers = mapOf(
-            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+       val userAgent =
+            "Mozilla/5.0 (Linux; Android 10; K) " +
+            "AppleWebKit/537.36 (KHTML, like Gecko) " +
+            "Chrome/139.0.0.0 Mobile Safari/537.36"
+        
+        val cookieManager = CookieManager.getInstance()
+        
+        val maxStreamCookies =
+            cookieManager.getCookie(url)
+                ?: cookieManager.getCookie("https://maxstream.video")
+                ?: ""
+        
+        Log.d("MAXSTREAM_DEBUG", "URL = $url")
+        Log.d("MAXSTREAM_DEBUG", "REFERER = $referer")
+        Log.d(
+            "MAXSTREAM_DEBUG",
+            "COOKIE MAXSTREAM PRESENTI = ${maxStreamCookies.isNotBlank()}"
+        )
+        
+        val headers = mutableMapOf(
+            "User-Agent" to userAgent,
             "Referer" to (referer ?: url)
         )
+        
+        if (maxStreamCookies.isNotBlank()) {
+            headers["Cookie"] = maxStreamCookies
+}
 
         val response = app.get(url, headers = headers)
+        
+        Log.d("MAXSTREAM_DEBUG", "STATUS = ${response.code}")
+        Log.d("MAXSTREAM_DEBUG", "FINAL URL = ${response.url}")
+        Log.d("MAXSTREAM_DEBUG", "HTML LENGTH = ${response.text.length}")
+        
         var html = response.text
 
         if (html.contains("eval(function(p,a,c,k,e,d)")) {
@@ -35,7 +65,11 @@ class MaxStream : ExtractorApi() {
         val matches = streamUrlRegex.findAll(html).map { it.value }.distinct().toList()
 
         for (streamUrl in matches) {
-            val isM3u8 = streamUrl.contains(".m3u8")
+            val isM3u8 = streamUrl.contains(".m3u8", ignoreCase = true)
+            Log.d(
+            "MAXSTREAM_DEBUG",
+            "STREAM = $streamUrl | M3U8=$isM3u8"
+        )
 
             if (isM3u8) {
                 // Utilizzo della funzione generateM3u8 nativa
