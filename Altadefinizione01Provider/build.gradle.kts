1479256ffb222dffc17b4cde7ba446fc681014c0
Danix160 <tiziotipo18@gmail.com>
2026-09-05 13:22:52 +0200

    Update build.gradle.kts

--- Altadefinizione01Provider/build.gradle.kts
+++ Altadefinizione01Provider/build.gradle.kts
@@ -21,7 +21,7 @@ cloudstream {
     extra["prefix"] = "Altadefinizione01"
     extra["displayName"] = "Altadefinizione01"
     
-    version = 16
+    version = 17
     description = "Altadefinizione"
     authors = listOf("Danix")
     
