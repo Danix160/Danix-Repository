e32b233d5e44eeba0adf96be359c0a1acba6ea3a
Danix160 <tiziotipo18@gmail.com>
2026-09-09 06:56:47 +0200

    Update build.gradle.kts

--- RiveStreamProvider/build.gradle.kts
+++ RiveStreamProvider/build.gradle.kts
@@ -3,15 +3,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 android {
     namespace = "com.rivestream"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 }
 
 tasks.withType<KotlinJvmCompile>().configureEach {
     compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_1_8)
-        freeCompilerArgs.add("-Xskip-metadata-version-check")
+        jvmTarget.set(JvmTarget.JVM_11)
     }
 }
-
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
