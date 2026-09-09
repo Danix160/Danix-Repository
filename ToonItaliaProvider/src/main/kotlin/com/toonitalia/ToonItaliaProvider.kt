7483021b6d584e69e2ba5a60db8f9b86a0d4ca2a
Danix160 <tiziotipo18@gmail.com>
2026-09-09 14:03:13 +0200

    Update ToonItaliaProvider.kt

--- ToonItaliaProvider/src/main/kotlin/com/toonitalia/ToonItaliaProvider.kt
+++ ToonItaliaProvider/src/main/kotlin/com/toonitalia/ToonItaliaProvider.kt
@@ -1,7 +1,7 @@
 package com.toonitalia
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.addTrailer
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.amap
