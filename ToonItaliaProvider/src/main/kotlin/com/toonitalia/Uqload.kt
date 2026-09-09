796072b1af4157496844019cabb5320fbfe50499
Danix160 <tiziotipo18@gmail.com>
2026-09-09 08:49:46 +0200

    Update Uqload.kt

--- ToonItaliaProvider/src/main/kotlin/com/toonitalia/Uqload.kt
+++ ToonItaliaProvider/src/main/kotlin/com/toonitalia/Uqload.kt
@@ -34,18 +34,46 @@ open class Uqload : ExtractorApi() {
     override var mainUrl: String = "https://www.uqload.com"
     override val requiresReferer = true
 
-    private val  srcRegex = Regex("""sources:.*"(.*?)".*""")  // would be possible to use the parse and find src attribute
+    private val srcRegex = Regex(
+    """sources\s*:\s*\[\s*["']([^"']+)["']""",
+    setOf(
+        RegexOption.IGNORE_CASE,
+        RegexOption.DOT_MATCHES_ALL
+    )
+)
 
     override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
-        with(app.get(url)) {  // raised error ERROR_CODE_PARSING_CONTAINER_UNSUPPORTED (3003) is due to the response: "error_nofile"
-            srcRegex.find(this.text)?.groupValues?.get(1)?.let { link ->
-                // Log.d("CS3debugUQload","decoded URL: $link")
-                callback.invoke(
+                url: String,
+                referer: String?,
+                subtitleCallback: (SubtitleFile) -> Unit,
+                callback: (ExtractorLink) -> Unit
+            ) {
+                println("[Uqload] URL ricevuto: $url")
+            
+                val response = app.get(
+                    url,
+                    referer = referer
+                )
+            
+                println("[Uqload] status=${response.code}")
+                println("[Uqload] finalUrl=${response.url}")
+            
+                val match = srcRegex.find(response.text)
+            
+                if (match == null) {
+                    println("[Uqload] SOURCES NON TROVATO")
+                    println(
+                        "[Uqload] HTML sample=" +
+                            response.text.take(1000)
+                    )
+                    return
+                }
+            
+                val link = match.groupValues[1]
+            
+                println("[Uqload] VIDEO TROVATO: $link")
+            
+                callback(
                     newExtractorLink(
                         source = name,
                         name = name,
