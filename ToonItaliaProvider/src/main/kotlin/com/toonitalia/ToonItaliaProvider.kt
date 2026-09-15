84463ec2f522eefce6f661d19d1f88f48382e895
Danix160 <tiziotipo18@gmail.com>
2026-09-15 15:31:49 +0200

    Update ToonItaliaProvider.kt

--- ToonItaliaProvider/src/main/kotlin/com/toonitalia/ToonItaliaProvider.kt
+++ ToonItaliaProvider/src/main/kotlin/com/toonitalia/ToonItaliaProvider.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.amap
-import com.lagradost.nicehttp.CloudflareKiller
+import com.lagradost.cloudstream3.network.CloudflareKiller
 import org.jsoup.nodes.Element
 import java.util.concurrent.ConcurrentHashMap
 
