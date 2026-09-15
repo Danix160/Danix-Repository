11ce9381580c9d3f9017af32445a83d4cd1e8e94
Danix160 <tiziotipo18@gmail.com>
2026-09-15 15:56:30 +0200

    Update GuardaPlayProvider.kt

--- GuardaPlayProvider/src/main/kotlin/com/guardaplay/GuardaPlayProvider.kt
+++ GuardaPlayProvider/src/main/kotlin/com/guardaplay/GuardaPlayProvider.kt
@@ -12,7 +12,7 @@ import java.net.URLEncoder
 
 class GuardaPlayProvider : MainAPI() {
 
-    override var mainUrl = "https://guardaplay.online"
+    override var mainUrl = "https://guardaplay.info"
     override var name = "GuardaPlay"
     override var lang = "it"
 
