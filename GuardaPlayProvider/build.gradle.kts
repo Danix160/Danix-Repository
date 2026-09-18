afa9824bf935b6468bfde63cd7a63823b267768e
Danix160 <tiziotipo18@gmail.com>
2026-09-18 12:49:55 +0200

    Update build.gradle.kts

--- GuardaPlayProvider/build.gradle.kts
+++ GuardaPlayProvider/build.gradle.kts
@@ -22,7 +22,7 @@ cloudstream {
     extra["prefix"] = "GuardaPlay"
     extra["displayName"] = "GuardaPlay"
     
-    version = 12
+    version = 13
     description = "GuardaPlay"
     authors = listOf("Danix")
     
