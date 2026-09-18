07714427d842e21949b847aaac03c852df61066e
Danix160 <tiziotipo18@gmail.com>
2026-09-18 10:18:52 +0200

    Create build.gradle.kts

---
+++ AltadefinizioneProvider/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+android {
+    namespace = "com.altadefinizione"
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_1_8)
+        freeCompilerArgs.add("-Xskip-metadata-version-check")
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
+
+cloudstream {
+    extra["prefix"] = "Altadefinizione"
+    extra["displayName"] = "Altadefinizione"
+    
+    version = 1
+    description = "Altadefinizione"
+    authors = listOf("Danix")
+    
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Cartoon")
+    requiresResources = false
+    language = "it"
+    iconUrl = "https://raw.githubusercontent.com/Danix160/Danix-Repository/refs/heads/master/Altadefinizione/logo.png"
+}
