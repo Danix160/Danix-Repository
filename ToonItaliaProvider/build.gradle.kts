6fa319a4817b61d662cd525d9f3474622d8cff97
Danix160 <tiziotipo18@gmail.com>
2026-09-22 23:44:45 +0200

    Update build.gradle.kts

--- ToonItaliaProvider/build.gradle.kts
+++ ToonItaliaProvider/build.gradle.kts
@@ -20,7 +20,7 @@ cloudstream {
     extra["prefix"] = "ToonItalia"
     extra["displayName"] = "ToonItalia"
 
-    version = 121
+    version = 122
     description = "Archivio di Anime e Cartoni animati in italiano da ToonItalia.xyz"
     authors = listOf("Danix")
 
